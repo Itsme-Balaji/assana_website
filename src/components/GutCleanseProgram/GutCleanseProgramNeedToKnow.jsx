042f6ab0b43f00808a5edae126f7e5250c2455e2
Balaji <balajipadmanaban591@gmail.com>
2025-10-04 12:47:20 +0530

    updated

--- src/components/GutCleanseProgram/GutCleanseProgramNeedToKnow.jsx
+++ src/components/GutCleanseProgram/GutCleanseProgramNeedToKnow.jsx
@@ -3,7 +3,7 @@ import plevic1 from '../../assets/images/plevic1.png'
 import plevic2 from '../../assets/images/plevic2.png'
 import plevic3 from '../../assets/images/plevic3.png'
   
-const GutCleanseProgramNeedToKnow = ({colonHydrotherapyPageData}) => {
+const GutCleanseProgramNeedToKnow = ({problems}) => {
   return (
     <section>
         <div className='max-w-[1600px] bg-[] m-auto overflow-hidden'>
@@ -11,13 +11,13 @@ const GutCleanseProgramNeedToKnow = ({colonHydrotherapyPageData}) => {
             <div className='grid grid-cols-1   lg:grid-cols-3 lg:grid-rows-2 gap-y-10 lg:gap-y-30 bg-[] w-[95%] lg:w-[85%] m-auto mt-10 lg:mt-25'>
                <div className='flex col-span-2 lg:col-span-1  justify-center lg:justify-start items-center bg-[] w-full order-1 lg:order-1'>
                     <div className='bg-[] relative flex justify-center h-90 sm:h-110 w-80 rounded-3xl' data-aos="fade-right" data-duration="1000">
-                        <img src={colonHydrotherapyPageData?.needToKnowImg1} className='h-full w-full rounded-3xl' alt='' />
-                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>{colonHydrotherapyPageData?.needToKnow1Heading}</h1>
+                        <img src={problems?.ColonHydrotherapy_Image} className='h-full w-full rounded-3xl' alt='' />
+                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>What Is Colon Hydrotherapy?</h1>
                     </div>
                </div>
                <div className='row-span-1 flex flex-col lg:justify-center col-span-2 bg-[] order-2 lg:order-2'>
-                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>{colonHydrotherapyPageData?.needToKnow1WhatisHeading}</h1>
-                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow1WhatisPara}</p>
+                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>What Is It?</h1>
+                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_ColonHydrotherapy}</p>
 
                </div>
 
@@ -28,19 +28,19 @@ const GutCleanseProgramNeedToKnow = ({colonHydrotherapyPageData}) => {
 
                
                <div className='col-span-2 flex flex-col lg:justify-center order-4 lg:order-3'>
-                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>{colonHydrotherapyPageData?.needToKnow2WhatisHeading}</h1>
-                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow2WhatisPara1}</p>
-                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow2WhatisPara2}</p>
-                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow2WhatisPara3}</p>
-                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow2WhatisPara4}</p>
-                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow2WhatisPara5}</p>
-                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow2WhatisPara6}</p>
+                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>What Is It?</h1>
+                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Benefits_ColonHydrotherapy_Para1}</p>
+                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Benefits_ColonHydrotherapy_Para2}</p>
+                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Benefits_ColonHydrotherapy_Para3}</p>
+                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Benefits_ColonHydrotherapy_Para4}</p>
+                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Benefits_ColonHydrotherapy_Para5}</p>
+                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Benefits_ColonHydrotherapy_Para6}</p>
 
                </div>
                <div className='flex col-span-2 lg:col-span-1 justify-center lg:justify-end items-center h-full bg-[#] order-3 lg:order-4'>
                     <div className='bg-[] relative flex justify-center h-90 sm:h-110 w-80 rounded-3xl' data-aos="fade-left" data-duration="1000">
-                        <img src={colonHydrotherapyPageData?.needToKnowImg2} className='h-full w-full rounded-3xl' alt='' />
-                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>{colonHydrotherapyPageData?.needToKnow2Heading}</h1>
+                        <img src={problems?.Benefits_ColonHydrotherapy_Image} className='h-full w-full rounded-3xl' alt='' />
+                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>Benefits of Colon Hydrotherapy</h1>
                          
                     </div>
                </div>
@@ -49,17 +49,17 @@ const GutCleanseProgramNeedToKnow = ({colonHydrotherapyPageData}) => {
 
                <div className='flex col-span-2 lg:col-span-1 justify-center lg:justify-start items-center order-5 bg-[]'>
                     <div className='bg-[] relative flex justify-center h-90 sm:h-110 w-80 rounded-3xl' data-aos="fade-right" data-duration="1000">
-                        <img src={colonHydrotherapyPageData?.needToKnowImg3} className='h-full w-full rounded-3xl' alt='' />
-                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>{colonHydrotherapyPageData?.needToKnow3Heading}</h1>
+                        <img src={problems?.Cutting_EdgeTechnology_Image} className='h-full w-full rounded-3xl' alt='' />
+                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>Our Cutting-Edge Technology</h1>
 
                     </div>
                </div>
                <div className='col-span-2 flex flex-col lg:justify-center order-6'>
-                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>{colonHydrotherapyPageData?.needToKnow3WhatisHeading}</h1>
-                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow3WhatisPara}</p>
+                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>What Is It?</h1>
+                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Cutting_EdgeTechnology}</p>
 
-                    <h1 className='text-[#EC7979] text-2xl sm:text-3xl font-[Raleway] font-semibold mt-9'>{colonHydrotherapyPageData?.needToKnow3HelpHeading}</h1>
-                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow3HelpPara}</p>
+                    <h1 className='text-[#EC7979] text-2xl sm:text-3xl font-[Raleway] font-semibold mt-9'>The System</h1>
+                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{problems?.Cutting_EdgeTechnology_System}</p>
 
                </div>
 
@@ -70,19 +70,19 @@ const GutCleanseProgramNeedToKnow = ({colonHydrotherapyPageData}) => {
 
                
                <div className='col-span-2 flex flex-col lg:justify-center order-8 lg:order-7'>
-                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>{colonHydrotherapyPageData?.needToKnow4WhatisHeading}</h1>
-                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow4WhatisPara1}</p>
-                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow4WhatisPara2}</p>
+                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>What Is It?</h1>
+                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Expect_DuringTreatment_para1}</p>
+                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Expect_DuringTreatment_para2}</p>
 
 
-                    <h1 className='text-[#EC7979] text-2xl sm:text-3xl font-[Raleway] font-semibold mt-9'>{colonHydrotherapyPageData?.needToKnow4HelpHeading}</h1>
-                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow4HelpPara}</p>
+                    <h1 className='text-[#EC7979] text-2xl sm:text-3xl font-[Raleway] font-semibold mt-9'>Post-Treatment Care</h1>
+                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Post_TreatmentCare}</p>
 
                </div>
                <div className='flex col-span-2 lg:col-span-1 justify-center  lg:justify-end items-center order-7 lg:order-8  bg-[]'>
                     <div className='bg-[] relative flex justify-center h-90 sm:h-110 w-80 rounded-3xl' data-aos="fade-left" data-duration="1000">
-                        <img src={colonHydrotherapyPageData?.needToKnowImg4} className='h-full w-full rounded-3xl' alt='' />
-                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>{colonHydrotherapyPageData?.needToKnow4Heading}</h1>
+                        <img src={problems?.Expect_DuringTreatment_Image} className='h-full w-full rounded-3xl' alt='' />
+                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>What to Expect During Your Treatment</h1>
                          
                     </div>
                </div>
@@ -95,16 +95,17 @@ const GutCleanseProgramNeedToKnow = ({colonHydrotherapyPageData}) => {
 
                 <div className='flex col-span-2 lg:col-span-1 justify-center lg:justify-start items-center order-9 bg-[]'>
                     <div className='bg-[] relative flex justify-center h-90 sm:h-110 w-80 rounded-3xl' data-aos="fade-right" data-duration="1000">
-                        <img src={colonHydrotherapyPageData?.needToKnowImg5} className='h-full w-full rounded-3xl' alt='' />
-                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>{colonHydrotherapyPageData?.needToKnow5Heading}</h1>
+                        <img src={problems?.Who_Benefit_ColonHydrotherapy_Image} className='h-full w-full rounded-3xl' alt='' />
+                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>Who Can Benefit from Colon Hydrotherapy?
+</h1>
                     </div>
                </div>
                <div className='col-span-2 flex flex-col lg:justify-center order-10'>
-                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>{colonHydrotherapyPageData?.needToKnow5WhatisHeading}</h1>
-                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow5WhatisPara1}</p>
-                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow5WhatisPara2}</p>
-                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow5WhatisPara3}</p>
-                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow5WhatisPara4}</p>
+                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>What Is It?</h1>
+                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{problems?.Who_Benefit_ColonHydrotherapy_Para1}</p>
+                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{problems?.Who_Benefit_ColonHydrotherapy_Para2}</p>
+                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{problems?.Who_Benefit_ColonHydrotherapy_Para3}</p>
+                    <p className='text-base sm:text-lg  font-[Raleway] mt-3 w-[100%]'>{problems?.Who_Benefit_ColonHydrotherapy_Para4}</p>
 
                </div>
 
@@ -115,17 +116,17 @@ const GutCleanseProgramNeedToKnow = ({colonHydrotherapyPageData}) => {
 
                
                <div className='col-span-2 flex flex-col lg:justify-center order-12 lg:order-11'>
-                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>{colonHydrotherapyPageData?.needToKnow6WhatisHeading}</h1>
-                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow6WhatisPara1}</p>
-                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow6WhatisPara2}</p>
-                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow6WhatisPara3}</p>
-                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{colonHydrotherapyPageData?.needToKnow6WhatisPara4}</p>
+                    <h1 className='text-2xl sm:text-3xl font-[Raleway] font-semibold'>What Is It?</h1>
+                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Comprehensive_Gut_CleanseProgram_para1}</p>
+                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Comprehensive_Gut_CleanseProgram_para2}</p>
+                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Comprehensive_Gut_CleanseProgram_para3}</p>
+                    <p className='text-base sm:text-lg font-[Raleway] mt-3 w-[100%]'>{problems?.What_is_Comprehensive_Gut_CleanseProgram_para4}</p>
 
                </div>
                <div className='flex col-span-2 lg:col-span-1 justify-center  lg:justify-end items-center order-11 lg:order-12  bg-[]'>
                     <div className='bg-[] relative flex justify-center h-90 sm:h-110 w-80 rounded-3xl' data-aos="fade-left" data-duration="1000">
-                        <img src={colonHydrotherapyPageData?.needToKnowImg6} className='h-full w-full rounded-3xl' alt='' />
-                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>{colonHydrotherapyPageData?.needToKnow6Heading}</h1>
+                        <img src={problems?.Comprehensive_Gut_CleanseProgram_Image} className='h-full w-full rounded-3xl' alt='' />
+                        <h1 className='text-2xl text-white font-bold absolute top-7 p-2 text-center'>Our Comprehensive Gut Cleanse Program</h1>
                          
                     </div>
                </div>
--- src/pages/GutCleanseProgram.jsx
+++ src/pages/GutCleanseProgram.jsx
@@ -24,36 +24,69 @@ const GutCleanseProgram = () => {
 
         const [chatBot, setChatBot] = useState(false);
 
-  const [colonHydrotherapyPageData, setcolonHydrotherapyPageData] = useState([]);
-              const [loading, setLoading] = useState(true);
+   const [banner, setBanner] = useState([]);
+  const [problems, setProblems] = useState([]);
+  const [whyAssana, setWhyAssana] = useState([]);
+
+  const [loading, setLoading] = useState(true);
   
-      
-          useEffect(()=>{
-              axios.get('https://assana-doctorwebsite-backend.vercel.app/api/colonHydrotherapyPage/dataGet')
-              .then((response) => {
-                  setcolonHydrotherapyPageData(response.data.data);
+  
+      useEffect(()=>{
+          axios.get('https://assana-server.vercel.app/api/colonHydrotherapyBanner/dataGet')
+          .then((response) => {
+              setBanner(response.data.data);
                     setLoading(false);
 
-      
-              })
-              .catch((error) => {
-              console.error("Error:", error);
+  
+          })
+          .catch((error) => {
+          console.error("Error:", error);
                     setLoading(false);
 
-              });
-      
-          },[])
+          });
 
-           if (loading || !colonHydrotherapyPageData) {
+
+          axios.get('https://assana-server.vercel.app/api/colonHydrotherapyProblems/dataGet')
+          .then((response) => {
+              setProblems(response.data.data);
+                    setLoading(false);
+
+  
+          })
+          .catch((error) => {
+          console.error("Error:", error);
+                    setLoading(false);
+
+          });
+
+          axios.get('https://assana-server.vercel.app/api/colonHydrotherapyWhyAssana/dataGet')
+          .then((response) => {
+              setWhyAssana(response.data.data);
+                    setLoading(false);
+
+  
+          })
+          .catch((error) => {
+          console.error("Error:", error);
+                    setLoading(false);
+
+          });
+  
+      },[])
+
+      console.log("banner",banner);
+      
+
+       if (loading || !banner || !problems || !whyAssana ) {
     return <Spinner />;  // <-- Show spinner until data loads
   }
 
   return (
     <>
      <Header />
-     <PlevicFloorBanner plevicFloorProblem={colonHydrotherapyPageData}/>
-     <GutCleanseProgramNeedToKnow colonHydrotherapyPageData={colonHydrotherapyPageData}/>
-     <WhyAssana colonHydrotherapyPageData={colonHydrotherapyPageData}/>
+     <PlevicFloorBanner banner={banner}/>
+     <GutCleanseProgramNeedToKnow problems={problems}/>
+     <WhyAssana whyAssana={whyAssana}/>
      <PlevicAssanaTreat />
      <FrequentlyQA />
      <PlevicBooking />
